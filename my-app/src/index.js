238aed75523b163a417ad9ad274264fed350a8c4
zareen-t <zareentasnim@outlook.com>
2023-05-31 00:01:36 -0700

    More zareen changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+// import aboutus from "./Home"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route exact path="/" element={<App />} />
+        {/* <Route path="/about" element={<aboutus />} /> */}
       </Routes>
     </BrowserRouter>
 );
